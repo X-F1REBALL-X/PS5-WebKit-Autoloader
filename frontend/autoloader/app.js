345287caea46f0971cc309ff969fed5aa6a228ff
X-F1REBALL-X <ohad.itzhak@gmail.com>
2026-09-20 23:25:22 +0300

    WK-AutoLoader

--- frontend/autoloader/app.js
+++ frontend/autoloader/app.js
@@ -901,7 +901,7 @@
   }
 
   function start() {
-    uiLog('PS5 WebKit Autoloader by X-F1REBALL-X', 'success');
+    uiLog('PS5-WebKit-Autoloader by X-F1REBALL-X', 'success');
     if (statusMsgEl) statusMsgEl.textContent = 'Jailbreak started';
     updateProgress(0, 'Jailbreak started');
     startProgressDriver();
