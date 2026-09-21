d641682dc086559f67752b01afe518f97cc249d0
X-F1REBALL-X <ohad.itzhak@gmail.com>
2026-09-21 04:21:38 +0300

    v1.6 skip install if up to date

--- include/app_installer.h
+++ include/app_installer.h
@@ -1,4 +1,7 @@
 #pragma once
 
+/* 1 = already installed at this build's version; 0 = missing/outdated. */
+int wkali_app_is_up_to_date(void);
+
 /* Installs/updates the "WebKit Autoloader" homescreen app if needed. */
 int wkali_install_app_if_needed(void);
