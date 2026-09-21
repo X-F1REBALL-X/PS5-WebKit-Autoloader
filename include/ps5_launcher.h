2d24763b0a2318d6bb041b077c4d024e29821e89
X-F1REBALL-X <ohad.itzhak@gmail.com>
2026-09-21 03:44:23 +0300

    v1.6

--- include/ps5_launcher.h
+++ include/ps5_launcher.h
@@ -6,3 +6,9 @@
  * @return 0 on success, -1 on failure.
  */
 int ps5_launch_browser(const char *uri);
+
+/**
+ * Return to the PS5 home screen (closes the WebKit browser cleanly).
+ * @return 0 on success, negative on failure.
+ */
+int ps5_return_to_home(void);
